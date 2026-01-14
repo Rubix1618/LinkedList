92358d6674670ba1026a65ead0cde48e5e8d3e77
Rubix1618 <sayondeep1618@gmail.com>
2026-01-15 00:34:08 +0530

    Slight correction in the circular singly linked list method and created object of the same in the main class

--- src/com/sayon/CLL.java
+++ src/com/sayon/CLL.java
@@ -1,160 +1,177 @@
 package com.sayon;
 
 public class CLL {
-    public class Node{
+
+    public class Node {
         int value;
         Node next;
-        Node(int value){
-            this.value=value;
-            this.next=null;
+
+        Node(int value) {
+            this.value = value;
+            this.next = null;
         }
     }
+
     private Node head;
-    public CLL(){
-        head=null;
+
+    public CLL() {
+        head = null;
     }
-    //Inserting functions
-    public void insert_first(int val){
-        Node node=new Node(val);
-        if(head==null){
-            head=node;
-            node.next=head;
+
+    // ===================== INSERT FUNCTIONS =====================
+    public void insert_first(int val) {
+        Node node = new Node(val);
+        if (head == null) {
+            head = node;
+            node.next = head;
             return;
         }
-        Node tail=head;
-        while(tail.next!=head){
-            tail=tail.next;
+        Node tail = head;
+        while (tail.next != head) {
+            tail = tail.next;
         }
-        tail.next=node;
-        node.next=head;
-        head=node;
+        node.next = head;
+        tail.next = node;
+        head = node;
     }
-    public void insert_last(int val){
-         Node node=new Node(val);
-        if(head==null){
-            head=node;
-            node.next=head;
+
+    public void insert_last(int val) {
+        Node node = new Node(val);
+        if (head == null) {
+            head = node;
+            node.next = head;
             return;
         }
-        Node tail=head;
-        while(tail.next!=head){
-            tail=tail.next;
+        Node tail = head;
+        while (tail.next != head) {
+            tail = tail.next;
         }
-        node.next=tail.next;
-        tail.next=node;
+        tail.next = node;
+        node.next = head;
     }
-    public void insert(int val,int pos){
-        Node node=new Node(val);
-        if(pos==0){
+
+    public void insert(int val, int pos) {
+        if (pos == 0) {
             insert_first(val);
             return;
         }
-        int totalnum=count();
-        if(pos==totalnum){
+        int totalnum = count();
+        if (pos == totalnum) {
             insert_last(val);
             return;
         }
-        Node temp=head;
-        for(int i=0;i<pos-1;i++){
-            temp=temp.next;
+        if (pos > totalnum || pos < 0) {
+            System.out.println("Invalid position");
+            return;
         }
-        node.next=temp.next;
-        temp.next=node;
+        Node temp = head;
+        for (int i = 0; i < pos - 1; i++) {
+            temp = temp.next;
+        }
+        Node node = new Node(val);
+        node.next = temp.next;
+        temp.next = node;
     }
-    //  counting the total number of nodes
-    public int count(){
-        Node temp=head;
-        int total=0;
-        while(temp!=null){
+
+    // ===================== COUNT FUNCTION =====================
+    public int count() {
+        if (head == null) return 0;
+        Node temp = head;
+        int total = 0;
+        do {
             total++;
-            temp=temp.next;
-        }
+            temp = temp.next;
+        } while (temp != head);
         return total;
     }
-    // Display Function
-    public void display(){
-        if(head==null){
+
+    // ===================== DISPLAY FUNCTION =====================
+    public void display() {
+        if (head == null) {
             System.out.println("The list is empty");
             return;
         }
-        Node temp=head;
-        while(temp!=null){
-            System.out.print(temp.value+ "  ");
-            temp=temp.next;
-        }
+        Node temp = head;
+        do {
+            System.out.print(temp.value + "  ");
+            temp = temp.next;
+        } while (temp != head);
         System.out.println();
     }
-    //Deleting function
-    public void delete_first(){
-        if(head==null){
-            System.out.println("List is Empty");
-            return;
-        }
-        if(head.next==head){
-            head=null;
-            return;
-        }
-        Node tail=head;
-        while(tail.next!=head){
-            tail=tail.next;
-        }
-        head=head.next;
-        tail.next=head;
-    }
-    public void delete_last(){
-         if(head==null){
-            System.out.println("List is Empty");
-            return;
-        }
-        if(head.next==head){
-            head=null;
-            return;
-        }
-        Node temp=head;
-        while(temp.next.next!=head){
-            temp=temp.next;
-        }
-        temp.next=head;
 
-    }
-    public void delete(int pos){
-         if(head==null){
-            System.out.println("List is Empty");
-            return;
-        }
-        if(pos==0){
-            delete_first();
-            return;
-        }
-        int totalnum=count();
-        if(pos==totalnum-1){
-            delete_last();
-            return;
-        }
-        Node temp=head;
-        for(int i=0;i<pos-1;i++){
-            temp=temp.next;
-        }
-        temp.next=temp.next.next;
-    }
-
-
-    //searching functions
-    public void search(int val){
-        if(head==null){
+    // ===================== DELETE FUNCTIONS =====================
+    public void delete_first() {
+        if (head == null) {
             System.out.println("List is empty");
             return;
         }
-        Node temp=head;
-        while(temp!=null && temp.value!=val){
-            temp=temp.next;
+        if (head.next == head) {
+            head = null;
+            return;
         }
-        if(temp==null){
-            System.out.println("Key not found");
+        Node tail = head;
+        while (tail.next != head) {
+            tail = tail.next;
         }
-        else{
-            System.out.println("Key found");
-        }
+        head = head.next;
+        tail.next = head;
     }
-    
+
+    public void delete_last() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        if (head.next == head) {
+            head = null;
+            return;
+        }
+        Node temp = head;
+        while (temp.next.next != head) {
+            temp = temp.next;
+        }
+        temp.next = head;
+    }
+
+    public void delete(int pos) {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        int totalnum = count();
+        if (pos < 0 || pos >= totalnum) {
+            System.out.println("Invalid position");
+            return;
+        }
+        if (pos == 0) {
+            delete_first();
+            return;
+        }
+        if (pos == totalnum - 1) {
+            delete_last();
+            return;
+        }
+        Node temp = head;
+        for (int i = 0; i < pos - 1; i++) {
+            temp = temp.next;
+        }
+        temp.next = temp.next.next;
+    }
+
+    // ===================== SEARCH FUNCTION =====================
+    public void search(int val) {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        Node temp = head;
+        do {
+            if (temp.value == val) {
+                System.out.println("Key found");
+                return;
+            }
+            temp = temp.next;
+        } while (temp != head);
+        System.out.println("Key not found");
+    }
 }
