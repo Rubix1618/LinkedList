0ff84f4aece1d78df2befd972530b57e09f04f02
Rubix1618 <sayondeep1618@gmail.com>
2026-01-15 00:25:42 +0530

    Changed the main function and created the object of the DLL class and used the Methods of DLL class for implementing Doubly Linked List

--- src/com/sayon/main.java
+++ src/com/sayon/main.java
@@ -2,7 +2,18 @@ package com.sayon;
 
 public class main {
     public static void main(String[] arg){
-        LL list=new LL();
+        // LL list=new  LL();
+        // list.insert_first(10);
+        // list.insert_last(20);
+        // list.insert_first(30);
+        // list.insert_first(40);
+        // list.display();
+        // list.insert(50,3);
+        // list.display();
+        // list.delete(3);
+        // list.display();
+        // list.search(1000);
+        DLL list=new  DLL();
         list.insert_first(10);
         list.insert_last(20);
         list.insert_first(30);
